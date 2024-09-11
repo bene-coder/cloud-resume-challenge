496c6af2a9a386e20b9a01158ce827f4473e1319
bene-coder <chidiebereernest8@gmail.com>
2024-09-11 14:06:00 +0100

    i made corrections

---
+++ app.js
@@ -0,0 +1,36 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
+import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+
+const client = new DynamoDBClient({});
+const docClient = DynamoDBDocumentClient.from(client);
+
+var counterContainer = document.querySelector(".website-counter");
+var resetButton = document.querySelector("#reset");
+var visitCount = localStorage.getItem("page_view");
+
+// Check if page_view entry is present
+if (visitCount) {
+  visitCount = Number(visitCount) + 1;
+  localStorage.setItem("page_view", visitCount);
+} else {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+}
+counterContainer.innerHTML = visitCount;
+
+// Adding onClick event listener
+resetButton.addEventListener("click", () => {
+  visitCount = 1;
+  localStorage.setItem("page_view", 1);
+  counterContainer.innerHTML = visitCount;
+});
+
+const command = new GetCommand({
+    TableName: "counter_tracker",
+    Key: {
+      CommonName: "record_id"
+    }
+  });
+
+const response = await docClient.send(command);
+console.log(response);
