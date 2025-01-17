ddd8a4b20a0f56dca4e4b70ee685ee385c41832e
bene-coder <onyiriaghabenedict@gmail.com>
2025-01-17 01:19:50 +0100

    Made new changes to the JS section

---
+++ backend/tests/README.md
@@ -0,0 +1,1 @@
+# Your tests lives here
--- js/app.js
+++ js/app.js
@@ -1,36 +1,20 @@
-import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
-import { DynamoDBDocumentClient, GetCommand } from "@aws-sdk/lib-dynamodb";
+window.addEventListener('DOMContentLoaded', (event) => {
+  getVisitCount();
+})
 
-const client = new DynamoDBClient({});
-const docClient = DynamoDBDocumentClient.from(client);
+const functionApi = '';
 
-var counterContainer = document.querySelector(".website-counter");
-var resetButton = document.querySelector("#reset");
-var visitCount = localStorage.getItem("page_view");
 
-// Check if page_view entry is present
-if (visitCount) {
-  visitCount = Number(visitCount) + 1;
-  localStorage.setItem("page_view", visitCount);
-} else {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-}
-counterContainer.innerHTML = visitCount;
-
-// Adding onClick event listener
-resetButton.addEventListener("click", () => {
-  visitCount = 1;
-  localStorage.setItem("page_view", 1);
-  counterContainer.innerHTML = visitCount;
-});
-
-const command = new GetCommand({
-    TableName: "counter_tracker",
-    Key: {
-      CommonName: "record_id"
-    }
+const getVisitCount = () => {
+  let count = 30;
+  fetch(functionApi).then(respons => {
+    return response.json()
+  }).then(response => {
+    console.log("Website called function API.");
+    count = response.count;
+    document.getElementById("counter").innerText = count;
+  }).catch(function (error) {
+    console.log(error);
   });
-
-const response = await docClient.send(command);
-console.log(response);
+  return count;
+}
