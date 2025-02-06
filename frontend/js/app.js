c2622c25fb845d9640dbf4f3d18d9bdb84bddcef
bene-coder <onyiriaghabenedict@gmail.com>
2025-02-06 13:57:20 +0100

    Made some changes to the frontend

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -2,12 +2,13 @@ window.addEventListener('DOMContentLoaded', (event) => {
   getVisitCount();
 })
 
-const functionApi = 'http://localhost:7071/api/GetResumeCounter';
+const functionApiUrl = 'https://getresumecounterr.azurewebsites.net/';
+const localFunctionApi = 'http://localhost:7071/api/GetResumeCounter';
 
 
 const getVisitCount = () => {
   let count = 30;
-  fetch(functionApi).then(respons => {
+  fetch(functionApiUrl).then(response => {
     return response.json()
   }).then(response => {
     console.log("Website called function API.");
